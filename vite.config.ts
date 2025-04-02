690d5caa75436636b4ef6fa2fd32d976dcda0655
EC2 Default User <ec2-user@ip-10-0-1-63.ec2.internal>
2025-04-02 13:45:54 +0000

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/absproxy/5173/',
+  server: {
+    allowedHosts: ['d6ua5ioavquoe.cloudfront.net']
+  }
 })
